f90fa69dc121272162f13155a5a8946a084fb059
autodl <zhentingqi@g.harvard.edu>
2023-10-18 03:16:12 +0800

    new

--- scripts/inspect_gz.py
+++ scripts/inspect_gz.py
@@ -1,13 +1,15 @@
 from xopen import xopen
+import json
 
 
 with xopen("/root/zhenting/ushape/data/qa_data/10_total_documents/nq-open-10_total_documents_gold_at_2.jsonl.gz") as fin:
     a = 0
-    import pdb; pdb.set_trace()
     for line in fin:
         # print(line)
+        d = json.loads(line)
+        print(d['answers'])
+        import pdb; pdb.set_trace()
         a += 1
-    import pdb; pdb.set_trace()
     print()
     
 print("DONE")
